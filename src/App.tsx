1988d61a3d5171fc8a6f99e3dda1f3155d75aade
DavidAntonioO <david-antonio.olmos@atos.net>
2022-05-20 08:25:17 -0700

    frist commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { useEffect } from "react";
+import React from 'react'
 import './App.css'
-import ProductTable from './components/ProductTable'
-import {products} from './components/Products.json'
-import {upgrades} from './components/UpgradesJson.json'
-import useRelatedCompanies from "./custom-hooks/useRelatedCompanies";
+
+
 
 function App(){
-  useEffect(() => {
-    console.log(products, upgrades);
-  },[])
-    return(
-      <main>
-        <ProductTable table ={products}/>
-        <useRelatedCompanies price = {products} />
-        
-      </main>
-    )
+  return(
+    <div>
+      <h2>Test</h2>
+    </div>
+   )
+    
    }
 
   export default App;
